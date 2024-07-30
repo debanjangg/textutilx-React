9239d793bd1bbd3c74e0334cd045cac3dac0832c
Debanjan <debanjan.g2002@gmail.com>
2024-07-30 13:15:58 +0530

    End of exercise 1 solutions

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,26 +18,33 @@ import React, {useState} from 'react'
         const handleLoClick = () => {
             setText(text.toLowerCase())
         }
-
+        
         const handleTitleClick = () => {
             const getTitleCase = (str) => {
                 let stArr = str.trim().split(' ');
                 let res = ""
                 for(let x=0; x<stArr.length; x++){
                     let word= stArr[x]
-                    res += word[0].toUpperCase() + word.slice(1) + " "
+                    res += word[0].toUpperCase() + word.slice(1).toLowerCase() + " "
                 }
                 return res.trim()
             }
             setText(getTitleCase(text))
-            //setText(text)
+        }
+        
+        const handleCopyClick = () => {
+            navigator.clipboard.writeText(text)
+        }
+        
+        const handleSpaces = () => {
+            setText(text.trim().replace(/[ ]+/g, ' '))
         }
         
         const getWords = () =>{
             if(text === '')
                 return 0
             else
-                return text.trim().split(' ').length
+                return text.trim().replace(/\s+/g, ' ').split(' ').length
         }
 
         return (
@@ -47,9 +54,11 @@ import React, {useState} from 'react'
                     <label htmlFor="textBox" className="form-label">{prop.heading}</label>
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="textBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleTitleClick}>Convert to Titlecase</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleTitleClick}>Convert to Titlecase</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleCopyClick}>Copy Text</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleSpaces}>Remove excess spaces</button>
             </div>
             <div className='my-4'>
                 <h2>Your text summary:</h2>
